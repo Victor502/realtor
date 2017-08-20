05fa66fac65737d1fcd432350ccc397722288b57
Victor Horton <victorahorton@yahoo.com>
2017-08-19 21:35:28 -0400

    added new img feature

---
+++ app.js
@@ -0,0 +1,41 @@
+$(document).foundation();
+
+$('.picture-grid').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    dots: true,
+    mobileFirst: true,
+      responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.main-caro').slick({
+    dots: true
+});
